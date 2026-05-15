e7287c648e653372ddc12c744bf0fa4860c830c3
Advait Hirlekar <advaithirlekar@gmail.com>
2026-05-15 06:45:47 +0000

    Initialized ADSL preprocessing and SDTM setup

--- question_2_adam/create_adsl.R
+++ question_2_adam/create_adsl.R
@@ -1,1 +1,36 @@
+#Importing the required libraries
+library(metacore)
+library(metatools)
+library(pharmaversesdtm)
+library(admiral)
+library(xportr)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(stringr)
+
+#initializing the input data
+dm <- pharmaversesdtm::dm
+vs <- pharmaversesdtm::vs
+ex <- pharmaversesdtm::ex
+ds <- pharmaversesdtm::ds
+ae <- pharmaversesdtm::ae
+suppdm <- pharmaversesdtm::suppdm
+
+#Replacing the missing blanks with NA wherever required
+dm <- convert_blanks_to_na(dm)
+vs <- convert_blanks_to_na(vs)
+ex <- convert_blanks_to_na(ex)
+ds <- convert_blanks_to_na(ds)
+ae <- convert_blanks_to_na(ae)
+suppdm <- convert_blanks_to_na(suppdm)
+
+dm_suppdm <- combine_supp(dm,suppdm)
+
+
+
+
+
+
+
 
