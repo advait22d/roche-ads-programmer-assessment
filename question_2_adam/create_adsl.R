44e9e68c12bb31480bd20e0e8911a8a48169a3b5
Advait Hirlekar <advaithirlekar@gmail.com>
2026-05-15 17:55:19 +0000

    Added Derived Variables from exposure data

--- question_2_adam/create_adsl.R
+++ question_2_adam/create_adsl.R
@@ -25,10 +25,47 @@ ds <- convert_blanks_to_na(ds)
 ae <- convert_blanks_to_na(ae)
 suppdm <- convert_blanks_to_na(suppdm)
 
+#Creating the combination of the primary and the supplement
 dm_suppdm <- combine_supp(dm,suppdm)
 
+#Starting to create adsl with the variables from DM data set
+adsl <- dm %>%
+  mutate(
+    AGEGR9 = case_when(
+      AGE<18 ~ "<18",
+      AGE>=18 & AGE<=50 ~ "18-50",
+      AGE>50 ~ ">50",
+      TRUE ~ NA_character_
+  ),AGEGR9N = case_when(
+    AGE<18 ~ 1,
+    AGE>=18 & AGE<=50 ~ 2,
+    AGE>50 ~ 3,
+    TRUE ~ NA_real_
+  ),
+  ITTFL = if_else(!is.na(ARM),"Y","N")
+  )
 
+#Deriving variables from EX data set. First getting EXSTDTM and EXSTTMF
+expo <- ex %>%
+  derive_vars_dtm(
+    dtc = EXSTDTC,
+    new_vars_prefix = "EXST",
+    time_imputation = "first"
+  )
 
+# Now adding the obtained EX variables to ADSL and applying our filter
+adsl <- adsl %>% 
+  derive_vars_merged(
+    dataset_add = expo,
+    filter_add=(EXDOSE>0 | (EXDOSE==0 & str_detect(EXTRT,"PLACEBO"))) &
+                 !is.na(EXSTDTM),
+               new_vars = exprs(TRTSDTM = EXSTDTM, TRTSTMF = EXSTTMF),
+               order = exprs(EXSTDTM, EXSEQ),
+                mode = "first",
+              by_vars = exprs(STUDYID,USUBJID)
+  )
+      
+  
 
 
 
