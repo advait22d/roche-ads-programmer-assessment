c7ff7b97d0d5f26e60ccfab65fb78d9db7d7e961
Advait Hirlekar <advaithirlekar@gmail.com>
2026-05-16 01:13:39 +0000

    Q# summary table completed

--- question_3_tlg/01_create_ae_summary_table.R
+++ question_3_tlg/01_create_ae_summary_table.R
@@ -1,1 +1,51 @@
 
+#Defining the log 
+sink("question_3_tlg/q3_summary_log.txt", split = TRUE)
+
+#Importing the required libraries
+
+library(ggplot2)
+library(tidyverse)
+library(gtsummary)
+library(pharmaverseadam)
+library(dplyr)
+
+#Reading the Adam data sets
+adae <- pharmaverseadam::adae
+adsl <- pharmaverseadam::adsl
+
+#Applying the filter for TRTEMFL mentioned in the question
+adae <- adae %>%
+  filter(TRTEMFL=="Y")
+
+#Building the summary table
+summary_adae <- adae %>%
+  tbl_hierarchical(
+    #These go inside the rows
+    variables = c(AESOC,AETERM),
+    #This forms the columns
+    by = ACTARM,
+    #counting based on subject id
+    id = USUBJID,
+    #taking denominator of the count as the safety population
+    denominator = adsl %>% filter(SAFFL == "Y"),
+    overall_row = TRUE,
+    label = "..ard_hierarchical_overall.." ~ "Treatment Emergent AEs"
+  )
+  
+#Creating the html output file
+summary_adae %>%
+  as_gt() %>%
+  gt::gtsave("question_3_tlg/ae_summary_table.html")
+
+
+cat("Question 3 summary creation completed successfully.\n")
+cat("Number of ADAE records after TEAE filtering:", nrow(adae), "\n")
+cat("Treatment groups in analysis:\n")
+print(unique(adae$ACTARM))
+
+cat("Preview of final TLG:\n")
+print(summary_adae)
+
+#Creating log
+sink()
